215a12779f36217392529e0e8cf0f1a01623071c
Phlipp Jeitner <p.jeitner@gmail.com>
2017-01-06 03:48:33 +0100

    cleanup

---
+++ html/common.js
@@ -0,0 +1,28 @@
+ 
+function extractHash() {
+	var table  = {};
+	var values = window.location.hash.substr(1);
+	values = values.split("&");
+	for (var i = 0; i < values.length; i++) {
+		var tuple = values[i].split("=");
+		var name  = tuple[0];
+		var value = tuple.length > 1 ? tuple[1] : null;
+		table[name] = value;
+	}
+	return table;
+}
+
+function formatDate(date) {
+	d = new Date(date * 1000);
+	return d.toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");
+}
+
+function formatDateTime(date) {
+	if (date == null) return "";
+	d = new Date(date * 1000);
+	return d.getDate() + "." + d.getMonth() + " " + d.toTimeString().replace(/.*(\d{2}:\d{2}):\d{2}.*/, "$1");
+}
+
+function time() {
+	return Math.round(new Date().getTime() / 1000);
+}
