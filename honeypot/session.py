0ae07cff31c84b0563bb6cd997992f053d140e40
Philipp Jeitner <p.jeitner@gmail.com>
2018-01-06 07:14:31 +0100

    Fix timeout

--- honeypot/session.py
+++ honeypot/session.py
@@ -61,8 +61,7 @@ class Session:
 				dbg("Stub downloader started: " + ip + ":" + str(port))
 			
 				try:
-					s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-					s.connect((ip, port), timeout=10)
+					s = socket.create_connection((ip, port), timeout=10)
 				
 					data = ""
 					while True:
